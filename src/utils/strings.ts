6a9d417b23ec6f5ccf36324a267091b3e0916b0b
Caleb <caleb@calebtheil.com>
2024-05-29 09:30:25 -0600

    ADDS: tests and improvement to strings utils
    Strings utils now correctly capitalizes like titles, using a map to
    determine if the word is typically not capitalized in english titles.
    Tests test both capCase function and dashToCapCase.

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,3 +1,26 @@
+const dontCap: { [key: string]: boolean } = {
+  "a": true,
+  "an": true,
+  "and": true,
+  "as": true,
+  "at": true,
+  "but": true,
+  "by": true,
+  "for": true,
+  "if": true,
+  "in": true,
+  "is": true,
+  "nor": true,
+  "of": true,
+  "on": true,
+  "or": true,
+  "so": true,
+  "the": true,
+  "to": true,
+  "up": true,
+  "yet": true
+};
+
 export const convDashedStrsToCapCase = (s: string): string => {
   const strs = s.split("-");
   return capCase(strs.join(" "))
@@ -9,6 +32,12 @@ export const capCase = (s: string): string => {
   let out = ""
   for (let i = 0; i < strs.length; i++) {
     let str = strs[i];
+    if (i !== 0 && dontCap[str]) {
+      // dont cap word
+      out += str + (i === strs.length - 1 ? "" : " ")
+      continue;
+    }
+    // do cap word
     out += str.charAt(0).toUpperCase() + str.substring(1) + (i === strs.length - 1 ? "" : " ")
   }
   return out;
