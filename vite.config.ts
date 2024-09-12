b5c75f5fba8f1d904697781d99b95af3f7c9bdb1
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-09-12 14:49:45 -0600

    UPDATE: vite.config and ci.yml git workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,33 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "ucdenver",
-    project: "digisign-web"
-  })],
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  server: {
-    proxy: {
-      '/api': {
-        target: ["https://lrc-api.wm.r.appspot.com", "http://localhost:8080", "https://lrc-storagebucket.s3.us-west-1.amazonaws.com"],
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
+  console.log("[vite.config.ts]: process.env: ", process.env.SENTRY_AUTH_TOKEN)
+
+  return defineConfig({
+    plugins: [react(), sentryVitePlugin({
+      org: "ucdenver",
+      project: "digisign-web",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: ["https://lrc-api.wm.r.appspot.com", "http://localhost:8080", "https://lrc-storagebucket.s3.us-west-1.amazonaws.com"],
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+      }
+    },
+
+    build: {
+      sourcemap: true
     }
-  },
+  })
 
-  build: {
-    sourcemap: true
-  }
-})
+}
