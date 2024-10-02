2c5e53d7a7c69c50d49c80b6145c13233312ecf9
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-10-02 12:37:46 -0600

    FIX: build error (rm useState for img => inline)

--- src/components/sessions/BGPhoto.tsx
+++ src/components/sessions/BGPhoto.tsx
@@ -8,30 +8,28 @@ type Props = {
 const BGPhoto = ({ session }: Props) => {
   var uri = session.photo || session.upPhoto?.url || "/card-fallback.jpg"
   const [loading, setLoading] = useState(true)
-  const [img, setImg] = useState<HTMLImageElement>(() => {
-    const image = new Image()
-    image.src = uri
-    image.onload = () => {
-      setLoading(false)
-    }
-    image.onerror = () => {
-      img.src = "/card-fallback.jpg"
-    }
-    image.className = "w-full h-full object-cover"
-    return image
-  })
 
+  const img = new Image()
+  img.src = uri
+  img.onload = () => {
+    setLoading(false)
+  }
+  img.onerror = () => {
+    img.src = "/card-fallback.jpg"
+
+  }
   return <motion.div
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     transition={{ duration: 1 }}
     exit={{ opacity: 0 }}
     className="absolute top-0 left-0 right-0 bottom-0 bg-gold-500 z-[-1]">
-    {/* {img} */}
+
     {loading ?
       <img className="w-full h-full object-cover blur" src="/card-fallback.jpg" />
       :
       <img className="w-full h-full object-cover" src={img.src} />
+
     }
   </motion.div>
 
